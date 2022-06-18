7691ae58fda79f37c9cf00c957c1e18255181cd3
Kartikdudeja <kartikdudeja21@gmail.com>
2022-06-18 23:28:33 +0530

    added logging module

--- python-app/main.py
+++ python-app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import logging
+
+# logging configuration
+logging.basicConfig(level=logging.INFO, filename='app.log', format="%(asctime)s: %(levelname)s: [%(process)d:%(processName)s] (%(filename)s-%(module)s.%(funcName)s): %(message)s")
 
 app = FastAPI()
 
+logger = logging.getLogger(__name__)
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    logger.info('Request Received for default Path')
+    return {"message": "Application is Running"}
