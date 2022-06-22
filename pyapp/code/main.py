1b7eefb4c547e6c806b0b987a739fe4d1756369e
Kartikdudeja <kartikdudeja21@gmail.com>
2022-06-22 09:00:55 +0530

    added db and py app service in docker-compose

--- pyapp/code/main.py
+++ pyapp/code/main.py
@@ -20,3 +20,18 @@ app.include_router(data.router)
 async def root():
     logger.info('Request Received for default Path')
     return {"message": "Application is Running"}
+
+# database connected logic
+
+# @app.on_event("startup")
+# async def startup():
+#     if not database.is_connected:
+#         await database.connect()
+#     # create a dummy entry
+#     await User.objects.get_or_create(email="test@test.com")
+
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     if database.is_connected:
+#         await database.disconnect()
